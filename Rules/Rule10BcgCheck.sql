4a0e7c6479cab30764b12153ebe107f8c888fb96
Amlan-cmd <amlan.mohanty@gds.ey.com>
2024-06-27 18:02:59 +0530

    nba

--- Rules/Rule10BcgCheck.sql
+++ Rules/Rule10BcgCheck.sql
@@ -3,5 +3,5 @@ Rule10BcgCheck as
  (Select b.marks,b.id,b.name,r.policestatus,r.validityid
 from bcg b join check r
 where b.maritialstatus = 'Single')
-//changed at 5 25, 27 6 24
+//changed at 5 25, 27 6 245
 
